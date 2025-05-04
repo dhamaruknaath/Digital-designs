b7652313a456eb27d7e7dc28d02c7df8ce8d9a9d
Dhamaruknaath <152205930+dhamaruknaath@users.noreply.github.com>
2025-05-04 22:27:44 +0530

    test-final

--- and-gate/and-gate.srcs/sources_1/new/andGate.v
+++ and-gate/and-gate.srcs/sources_1/new/andGate.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/04/2025 09:59:05 PM
-// Design Name: 
-// Module Name: andGate
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module andGate(
 x,y,f
